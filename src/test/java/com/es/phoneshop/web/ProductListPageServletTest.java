4218eaf6f087c801a0411cd14cc1f05ad3ed265d
Alex <iamavtoritet24.11@yandex.ru>
2021-06-16 11:32:36 +0300

    Fixed ProductListPageServletTest

--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -1,5 +1,9 @@
 package com.es.phoneshop.web;
 
+import com.es.phoneshop.model.recentviews.RecentView;
+import com.es.phoneshop.model.recentviews.RecentViewService;
+import com.es.phoneshop.model.sortenum.SortField;
+import com.es.phoneshop.model.sortenum.SortOrder;
 import lombok.SneakyThrows;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +16,7 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import static org.mockito.ArgumentMatchers.*;
@@ -20,30 +25,40 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductListPageServletTest {
-	@Mock
-	private HttpServletRequest request;
-	@Mock
-	private HttpServletResponse response;
-	@Mock
-	private RequestDispatcher requestDispatcher;
-	@Mock
-	private ServletConfig config;
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private RequestDispatcher requestDispatcher;
+    @Mock
+    private ServletConfig config;
+    @Mock
+    private RecentViewService recentViewService;
+    @Mock
+    private HttpSession session;
 
-	private ProductListPageServlet servlet = new ProductListPageServlet();
+    private ProductListPageServlet servlet = new ProductListPageServlet();
 
-	@SneakyThrows
-	@Before
-	public void setup() {
-		servlet.init(config);
-		when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
-	}
+    @SneakyThrows
+    @Before
+    public void setup() {
+        servlet.init(config);
+        when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+        when(request.getSession()).thenReturn(session);
+        when(recentViewService.getRecentView(request.getSession())).thenReturn(new RecentView());
+        when(request.getParameter(RequestParameter.QUERY)).thenReturn("Siemens");
+        when(request.getParameter(RequestParameter.SORT)).thenReturn(String.valueOf(SortField.DESCRIPTION));
+        when(request.getParameter(RequestParameter.ORDER)).thenReturn(String.valueOf(SortOrder.ASC));
+    }
 
-	@Test
-	public void testDoGet() throws ServletException, IOException {
-		servlet.doGet(request, response);
-		verify(requestDispatcher).forward(request, response);
-		verify(request).setAttribute(eq("products"), any());
-	}
+    @Test
+    public void testDoGet() throws ServletException, IOException {
+        servlet.doGet(request, response);
+        verify(requestDispatcher).forward(request, response);
+        verify(request).setAttribute(eq(RequestParameter.PRODUCTS), any());
+        verify(request).setAttribute(eq(RequestParameter.RECENT_VIEWS), any());
+    }
 
 
 }
