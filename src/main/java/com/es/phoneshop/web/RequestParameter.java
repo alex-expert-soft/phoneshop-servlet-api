964b002ac65ad36e9bcf3a9cc76a378153f267e0
Alex <iamavtoritet24.11@yandex.ru>
2021-06-23 10:07:18 +0300

    Added new request parameters

--- src/main/java/com/es/phoneshop/web/RequestParameter.java
+++ src/main/java/com/es/phoneshop/web/RequestParameter.java
@@ -3,8 +3,8 @@ package com.es.phoneshop.web;
 public class RequestParameter {
     public static final String MESSAGE = "message";
     public static final String ERROR = "error";
-    public static final String CART = "message";
-    public static final String RECENT_VIEWS = "recentViews";
+    public static final String CART = "cart";
+    public static final String RECENTLY_VIEWED = "recentlyViewed";
     public static final String PRODUCT = "product";
     public static final String PRODUCTS = "products";
     public static final String QUERY = "query";
@@ -13,4 +13,9 @@ public class RequestParameter {
     public static final String QUANTITY = "quantity";
     public static final String PRODUCT_ID = "productId";
     public static final String ERRORS = "errors";
+    public static final String PDP = "PDP";
+    public static final String PLP = "PLP";
+    public static final String REDIRECT = "redirect";
+    public static final String PLP_PAGE = "/products";
+    public static final String PDP_PAGE = "/products/";
 }
