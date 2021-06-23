7c97da28218825d85117bd9d998b2d35a39fffd3
Alex <iamavtoritet24.11@yandex.ru>
2021-06-23 10:05:33 +0300

    Method parameters changed

--- src/main/java/com/es/phoneshop/dao/ProductDao.java
+++ src/main/java/com/es/phoneshop/dao/ProductDao.java
@@ -1,8 +1,6 @@
 package com.es.phoneshop.dao;
 
 import com.es.phoneshop.model.product.Product;
-import com.es.phoneshop.model.sortenum.SortField;
-import com.es.phoneshop.model.sortenum.SortOrder;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -10,7 +8,7 @@ import java.util.List;
 public interface ProductDao {
     Product getProduct(Long id);
 
-    List<Product> findProducts(String searchQuery, SortField sortField, SortOrder sortOrder);
+    List<Product> findProducts(String searchQuery, String sortField, String sortOrder);
 
     void save(Product product);
 
