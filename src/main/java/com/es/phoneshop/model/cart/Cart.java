a3d52978f10620797b5e4bf7300aedb07057e812
Alex <iamavtoritet24.11@yandex.ru>
2021-06-23 09:53:59 +0300

    Add total cost & currency in Cart

--- src/main/java/com/es/phoneshop/model/cart/Cart.java
+++ src/main/java/com/es/phoneshop/model/cart/Cart.java
@@ -4,8 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
+import java.util.Locale;
 
 @Getter
 @Setter
@@ -13,6 +16,8 @@ public class Cart implements Serializable {
     private List<CartItem> items;
 
     private int totalQuantity;
+    private BigDecimal totalCost;
+    private final Currency currency = Currency.getInstance(Locale.US);
 
     public Cart() {
         this.items = new ArrayList<>();
