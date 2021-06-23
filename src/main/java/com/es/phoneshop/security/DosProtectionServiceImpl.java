dee89b62dd0e920ff903750cd9374471e099b35a
Alex <iamavtoritet24.11@yandex.ru>
2021-06-23 13:10:36 +0300

    Added DosFilter

---
+++ src/main/java/com/es/phoneshop/security/DosProtectionServiceImpl.java
@@ -0,0 +1,42 @@
+package com.es.phoneshop.security;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DosProtectionServiceImpl implements DosProtectionService {
+
+    private static volatile DosProtectionServiceImpl instance;
+
+    private DosProtectionServiceImpl() {
+    }
+
+    public static DosProtectionServiceImpl getInstance() {
+        if (instance == null) {
+            synchronized (DosProtectionServiceImpl.class) {
+                if (instance == null) {
+                    instance = new DosProtectionServiceImpl();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private static final long THRESHOLD = 10;
+
+    private final Map<String, Long> countMap = new ConcurrentHashMap<>();
+
+    @Override
+    public boolean isAllowed(String ip) {
+        Long count = countMap.get(ip);
+        if (count == null) {
+            count = 1L;
+        } else {
+            if (count > THRESHOLD) {
+                return false;
+            }
+            count++;
+        }
+        countMap.put(ip, count);
+        return true;
+    }
+}
