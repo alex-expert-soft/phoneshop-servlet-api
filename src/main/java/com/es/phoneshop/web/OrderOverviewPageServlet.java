1f31eec725f01a8e3b2aea493763a2edcfc29d49
Alex <iamavtoritet24.11@yandex.ru>
2021-06-23 13:14:31 +0300

    Added OrderOverview

---
+++ src/main/java/com/es/phoneshop/web/OrderOverviewPageServlet.java
@@ -0,0 +1,28 @@
+package com.es.phoneshop.web;
+
+import com.es.phoneshop.model.order.service.DefaultOrderServiceImpl;
+import com.es.phoneshop.model.order.service.OrderService;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class OrderOverviewPageServlet extends HttpServlet {
+    private OrderService orderService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        orderService = DefaultOrderServiceImpl.getInstance();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String orderId = request.getPathInfo().substring(1);
+        request.setAttribute(RequestParameter.ORDER, orderService.getOrder(Long.valueOf(orderId)));
+        request.getRequestDispatcher("/WEB-INF/pages/orderOverview.jsp").forward(request, response);
+    }
+}
